5ad35381691ee7f3e7ae6835bbeba1401d94e853
Red Desing <migueltarazona84@gmail.com>
2026-02-08 18:29:58 -0400

    Limpieza de iconos para estabilidad PWA

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,6 @@ const ASSETS_TO_CACHE = [
     'script.js',
     'templates.js',
     'manifest.json',
-    'icons/icon-192.png',
-    'icons/icon-512.png',
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
 ];
 
